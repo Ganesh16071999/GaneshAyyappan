5f57a48315d63de1cb1e2ee256e693da7a493d3c
Ganesh16071999 <100123059+Ganesh16071999@users.noreply.github.com>
2022-04-27 10:53:34 +0530

    Add files via upload

---
+++ FindPeekLeetCode.java
@@ -0,0 +1,18 @@
+package com.prog;
+
+public class FindPeekLeetCode {
+	 public int findPeakElement(int[] nums) {
+	        int ans=0;
+	        for(int i=0;i<nums.length-1;i++){
+	            if(nums[i]<nums[i+1]){
+	                ans=i+1;
+	            }
+	            else{
+	                return 0;
+	            }
+	        }
+	        return ans;
+	        
+	    }
+
+}
